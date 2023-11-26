e77eeb1e6cf9de8a212bce91af2676be9ee8fcdd
scott <scott.unity@skig.tech>
2023-11-26 15:52:59 +0800

    add grpc err trans

---
+++ xerr/translate.go
@@ -0,0 +1,29 @@
+package xerr
+
+import (
+	"context"
+	"fmt"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func TransError(ctx context.Context, err error) error {
+	if isGrpcError(err) {
+		if status.Code(err) >= codes.Canceled && status.Code(err) <= codes.Unauthenticated {
+			return status.Error(status.Code(err), "common.grpc_err_"+fmt.Sprint(status.Code(err)))
+		}
+	}
+	return err
+}
+
+func isGrpcError(err error) bool {
+	if err == nil {
+		return false
+	}
+
+	_, ok := err.(interface {
+		GRPCStatus() *status.Status
+	})
+
+	return ok
+}
